85046c5ac6af8c0abbd8f8bc8de13a0af9a53347
Jonathan East <jonathan_east@ymail.com>
2020-04-06 00:35:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ class TicTacToe
   end
 
   def valid_move?(index)
-    @board[!position_taken?(index)] && index.between(0,8) ? true : false
+    !position_taken?(index) && index.between(0,8) ? true : false
   end
 
 
