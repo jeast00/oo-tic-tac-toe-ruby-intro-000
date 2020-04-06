d7a6a7eebcfc7ba92b1b45509e078fa7116e3490
Jonathan East <jonathan_east@ymail.com>
2020-04-06 00:38:56 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,5 +38,15 @@ class TicTacToe
     !position_taken?(index) && index.between?(0,8) ? true : false
   end
 
+  def turn_count
+    counter = 0
+    @board.each do |space|
+      if space == "X" || space == "O"
+        counter += 1
+      end
+    end
+    return counter
+  end
+
 
 end
